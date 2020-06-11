41ea0c76f5adf60c1c205aad289aae3e8b0e0d9b
JisooJang <ppuagirls@naver.com>
2020-06-12 02:19:22 +0900

    feat: add cors setting in AppConfig class

--- src/main/java/com/example/user/config/AppConfig.java
+++ src/main/java/com/example/user/config/AppConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
     @Bean
     public Executor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
@@ -19,5 +21,12 @@ public class AppConfig {
         executor.setThreadNamePrefix("GithubLookup-");
         executor.initialize();
         return executor;
+    }
+
+    // CORS 설정.
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/users/github/sync")
+                .allowedOrigins("http://localhost:18080");
     }
 }
