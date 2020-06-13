6518fbbe9d33fefaf7aa9766eff9bb9ad289fae6
JisooJang <ppuagirls@naver.com>
2020-06-13 21:41:33 +0900

    feat: add validateNullCheck method in SignUpUser class

--- src/main/java/com/example/user/payload/SignUpUser.java
+++ src/main/java/com/example/user/payload/SignUpUser.java
@@ -2,10 +2,15 @@ package com.example.user.payload;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.logging.log4j.util.Strings;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
 
 @Getter
 @Setter
 public class SignUpUser {
+    @NotEmpty(message ="email should be not empty.")
     private String email;
     private String lastName; // 이름
     private String firstName; // 성
@@ -15,6 +20,15 @@ public class SignUpUser {
     private int birthDay; // 생일 일
     private int birthYear; // 생일 연도
 
-    private boolean marketing; // 마케팅 메시지 수신 여부
+    private Boolean marketing; // 마케팅 메시지 수신 여부
 
+    public boolean validateNullCheck() {
+        if(Strings.isBlank(email) || Strings.isBlank(lastName) || Strings.isBlank(firstName) || Strings.isBlank(password)) {
+            return false;
+        }
+        if(marketing == null) {
+            return false;
+        }
+        return true;
+    }
 }
