24b920dd4f44f02c93e25ab34ca647bfb5d4201d
JisooJang <ppuagirls@naver.com>
2020-07-07 11:53:16 +0900

    fix: delete @CreatedDate, @LastModifiedDate annotation in BaseEntity class

--- src/main/java/com/example/user/domain/BaseEntity.java
+++ src/main/java/com/example/user/domain/BaseEntity.java
@@ -3,8 +3,6 @@ package com.example.user.domain;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
@@ -13,12 +11,11 @@ import java.time.LocalDateTime;
 @Data
 @MappedSuperclass
 public class BaseEntity {
-    @Column(nullable = false, updatable = false)
-    @CreatedDate
     @CreationTimestamp
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @LastModifiedDate
     @UpdateTimestamp
+    @Column(nullable = true, updatable = true)
     private LocalDateTime updatedAt;
 }
