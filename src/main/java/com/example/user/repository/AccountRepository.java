075b00732d0f58df44b406f5005bbec94c87995f
JisooJang <ppuagirls@naver.com>
2020-07-11 19:02:18 +0900

    refactor: refactor repository

--- src/main/java/com/example/user/repository/AccountRepository.java
+++ src/main/java/com/example/user/repository/AccountRepository.java
@@ -1,6 +1,6 @@
 package com.example.user.repository;
 
-import com.example.user.domain.Account;
+import com.example.user.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
--- src/main/java/com/example/user/repository/MemberRepository.java
+++ src/main/java/com/example/user/repository/MemberRepository.java
@@ -1,6 +1,6 @@
 package com.example.user.repository;
 
-import com.example.user.domain.Member;
+import com.example.user.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
