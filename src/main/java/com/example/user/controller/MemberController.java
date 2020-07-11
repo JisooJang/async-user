2bbafa93abb3c859c97a6f1e08611589cef67297
JisooJang <ppuagirls@naver.com>
2020-07-11 19:01:21 +0900

    refactor: refactor MemberController class

--- src/main/java/com/example/user/controller/MemberController.java
+++ src/main/java/com/example/user/controller/MemberController.java
@@ -1,14 +1,12 @@
 package com.example.user.controller;
 
 import com.example.user.payload.GitHubLookupUser;
-import com.example.user.payload.GithubAccessTokenRequest;
 import com.example.user.payload.UserModel;
 import com.example.user.service.GitHubLookupService;
 import com.example.user.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
@@ -17,19 +15,18 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
+@RequestMapping("/api/v1/user")
 public class MemberController {
     private final GitHubLookupService gitHubLookupService;
     private final MemberService memberService;
-    private final RestTemplate restTemplate;
 
     public MemberController(GitHubLookupService gitHubLookupService, MemberService memberService) {
         this.gitHubLookupService = gitHubLookupService;
         this.memberService = memberService;
-        this.restTemplate = new RestTemplate();
     }
 
     @CrossOrigin(origins = {"http://localhost:18080"}) // 명시한 origin에서 현재 서버의 아래 URI 요청을 허용한다. Controller 레벨에서도 설정 가능.
-    @PostMapping("/api/v1/users/github/async")
+    @PostMapping("/github/async")
     public ResponseEntity<List<GitHubLookupUser>> getUsers(@RequestBody Set<String> users) throws InterruptedException {
         long start = System.currentTimeMillis();
         List<CompletableFuture<GitHubLookupUser>> futureList = new ArrayList<>();
@@ -46,7 +43,7 @@ public class MemberController {
         return ResponseEntity.ok(usersResult);
     }
 
-    @PostMapping("/api/v1/users/github/sync")
+    @PostMapping("/github/sync")
     public ResponseEntity<List<GitHubLookupUser>> getUsersSync(@RequestBody Set<String> users) throws InterruptedException {
         long start = System.currentTimeMillis();
         List<GitHubLookupUser> usersResult = new ArrayList<>();
@@ -58,19 +55,19 @@ public class MemberController {
         return ResponseEntity.ok(usersResult);
     }
 
-    @PostMapping("/api/v1/user/test")
+    @PostMapping("/test")
     public ResponseEntity<UserModel> testUser(@RequestBody UserModel user) {
         return ResponseEntity.ok(user);
     }
 
-    @GetMapping("/api/v1/login/oauth2/github")
+    @GetMapping("/login/oauth2/github")
     public ResponseEntity<?> getGithubLoginURL(HttpServletRequest request) {
         // 1. Request a user's GitHub identity
         String sessionId = request.getSession().getId();
         return ResponseEntity.ok(gitHubLookupService.getGithubLoginURL(sessionId));
     }
 
-    @GetMapping("/api/v1/login/oauth2/github/callback")
+    @GetMapping("/login/oauth2/github/callback")
     public ResponseEntity<?> githubLoginCallback(HttpServletRequest request, @RequestParam String code, @RequestParam String state) {
         String sessionId = request.getSession().getId();
         String accessToken = gitHubLookupService.getGithubAccessToken(sessionId, code, state);
