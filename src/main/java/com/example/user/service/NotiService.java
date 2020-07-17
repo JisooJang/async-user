1c764c5e9a35168c63274d11322270a7d0a09a07
JisooJang <ppuagirls@naver.com>
2020-07-18 01:23:10 +0900

    fix: delete async annotation in NotiService

--- src/main/java/com/example/user/service/NotiService.java
+++ src/main/java/com/example/user/service/NotiService.java
@@ -4,7 +4,6 @@ import com.example.user.payload.EmailSend;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
